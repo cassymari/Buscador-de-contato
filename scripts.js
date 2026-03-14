de1544bb90703aefec28f1585c0f327e75b772ad
cassymari <nascimentocassiane552@gmail.com>
2026-03-14 12:37:55 -0300

    Corrigindo erro

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const lista = document.getElementById("lista-contatos")
 const inputBusca = document.getElementById("buscar")
 
@@ -141,34 +141,4 @@ botaoTema.textContent = "🌙 Modo escuro"
 }
 
 })
-=======
-let p = document.querySelector("p")
-let input = document.querySelector("input")
 
-const contacts = [
-    { name: 'Camila', number: '(21) 99685-2415' },
-    { name: 'Cristiane', number: '(21) 99584-7152' },
-    { name: 'Felipe', number: '(21) 99685-5274' },
-    { name: 'Jusefa', number: '(21) 99685-3333' },
-    { name: 'Isabella', number: '(21) 99684-7523' },
-]
-
-function search() {
-    let found = false
-    let value = input.value.trim().toLowerCase()
-
-    for (let i = 0; i < contacts.length; i++) {
-        if (value === contacts[i].name.toLowerCase()) {
-            p.innerHTML = `Contato encontrado:<br>
-            Nome: ${contacts[i].name}<br>
-            Tel: ${contacts[i].number}`
-            found = true
-            break
-        }
-    }
-
-    if (!found) {
-        p.innerHTML = "Contato não encontrado, tente novamente!"
-    }
-}
->>>>>>> fc602d3c98d1e505cccc9b0b818511842bb7e436
